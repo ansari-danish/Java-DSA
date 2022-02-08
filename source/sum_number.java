10ee48b5ab0d8b84b500d70b54d2ad6148838360
Danish <80449860+ansari-danish@users.noreply.github.com>
2022-02-08 09:12:30 +0530

    add a new file

---
+++ source/sum_number.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class some {
+    public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+        System.out.println("give a number");
+String a = s.nextLine();
+        System.out.println("your number is"+" "+ a);
+    }
+}
