8c4a0d11f211ceb7fa09ee65d5520ea5a5e04099
Ansari Danish <dan71ish@gmail.com>
2022-03-02 22:14:31 +0530

    Some basic thing solve

---
+++ source/usersum.java
@@ -0,0 +1,16 @@
+//Take two numbers and print the sum of both.
+import java.util.Scanner;
+
+public class usersum {
+    public static void main(String[] args) {
+        Scanner ap= new Scanner (System.in);
+        int a ;
+        System.out.println("Enter first number");
+        a = ap.nextInt();
+        int b;
+        System.out.println("Enter Second number");
+        b = ap.nextInt();
+        int c = a+b;
+        System.out.println(c);
+            }
+}
