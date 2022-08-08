f62f48005ae59c6a4ba29f5092e22f51a88ed2e0
Danish <dan71ish@gmail.com>
2022-08-08 13:41:17 +0530

    Going to start learning journey again

--- source/basic-question/sum_number.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class some {
-    public static void main(String[] args) {
-Scanner s = new Scanner(System.in);
-        System.out.println("give a number");
-String a = s.nextLine();
-        System.out.println("your number is"+" "+ a);
-    }
-}
