03e9b0a23503eeebb34b16b1614aa21e3f895713
Danish <dan71ish@gmail.com>
2022-10-06 12:23:58 +0530

    Binary Search

---
+++ Assignment/07 - Binary Search Questions/InfiniteArray.java
@@ -0,0 +1,33 @@
+public class InfiniteArray {
+    public static void main(String[] args) {
+        int []arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int target = 10;
+        System.out.println(bound(arr,target));
+    }
+    static int bound(int[]arr,int target){
+        int start = 0;
+        int end = 1;
+        if(target < arr[start]){
+            return -1;
+        }
+        while(target>arr[end]){
+            int temp = end +1;
+            end = end + (end - start +1)*2;
+            start = temp;
+        }
+        return binarySearch(arr,target,start,end);
+    }
+    static int binarySearch(int[]arr,int target, int start,int end){
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(target > arr[mid])
+                start = mid+1;
+            else if(target < arr[mid])
+                end = mid-1;
+            else
+                return mid;
+        }
+        return -1;
+
+    }
+}
