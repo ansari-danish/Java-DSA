ab0d654e7ef1a407c079401e7064757ea1ca0d81
Danish <dan71ish@gmail.com>
2022-12-04 00:57:00 +0530

    Started Dsa again

--- Assignment/07 - Binary Search Questions/RotatedArray.java
+++ Assignment/07 - Binary Search Questions/RotatedArray.java
@@ -1,46 +1,46 @@
 public class RotatedArray {
     public static void main(String[] args) {
-        int []arr = {3,4,5,6,7,8,0,1,2};
+        int []arr = {3,4,5,6,7,8,9,0,1,2};
         int target = 2;
-        int pivot =Findpivot(arr);
-        if(pivot==-1 ) {
-            System.out.println(binarysearch(arr, target, 0, arr.length - 1));
-        }
-        if(target ==arr[pivot]) {
-            System.out.println(pivot);
-        }
-        if(target > arr[0]){
-            System.out.println(binarysearch(arr,target,0,pivot-1));
-        }
-        else
-            System.out.println(binarysearch(arr,target,pivot+1,arr.length-1));
     }
-    static int Findpivot(int[]arr){
+    public int search (int []arr, int target){
+        int pivot = Pivot(arr);
+        if(pivot == -1){
+            return BinarySearch(arr,target,0,arr.length -1 );
+        }
+        if(arr[pivot] == target)
+            return pivot;
+        if(target >= arr[0]) {
+            return BinarySearch(arr, target, 0, pivot - 1);
+        }
+        return BinarySearch(arr,target,pivot + 1,arr.length - 1);
+    }
+    static int Pivot(int []arr){
         int start = 0;
-        int end = arr.length-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(mid<end && arr[mid]>arr[mid+1])
-                return mid;
-            if(mid>start && arr[mid]<arr[mid-1])
-                return mid-1;
-            if(arr[mid] <= arr[start])
-                end = mid-1;
-            else
-                start = mid+1;
-            }
+        int end = arr.length - 1;
+        while(start <= end){
+         int mid = start + (end - start)/2;
+         if(mid<end && arr[mid] > arr[mid + 1])
+             return mid;
+         if(mid>start && arr[mid] < arr[mid -1])
+             return mid;
+         if(arr[mid] <= arr[start])
+             end = mid - 1;
+         else
+             start = mid + 1;
+      }
         return -1;
     }
-    static int binarysearch(int[]arr,int target, int start , int end){
-    while (start<=end){
-        int mid= start +(end-start)/2;
-        if(target<arr[mid])
-        end = mid-1;
-        if(target > arr[mid])
-            start= mid+1;
-        if(target == arr[mid])
-            return mid;
+    static int BinarySearch(int []arr , int target, int start ,int end ){
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(target<arr[mid])
+                end = mid - 1;
+            else if ( target > arr[mid] )
+                start = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
     }
-    return -1;
-   }
 }
